8ab41b641d394bbfd053fcbe0c416bc43200ebd2
lurbano <lurbano@earthsciweb.org>
2021-03-04 14:06:58 -0600

    fix

--- php/saveData.php
+++ php/saveData.php
@@ -1,8 +1,8 @@
 <?php
 echo htmlspecialchars("Hello: " . $_GET["text"]);
 
-$data = {"user": "lurbano", "text": $_GET["text"]};
-
+// $data = {"user": "lurbano", "text": $_GET["text"]};
+$data = $_GET["text"];
 $dataString = json_encode($data);
 
 file_put_contents("dataFile.json", $dataString);
